8dd5b9bfdbe2718612984b71473eaa9cc5fd472f
Ayush <ayushkhamar1112001@gmail.com>
2026-07-18 12:16:01 +0530

    only in dev server frintend will display

--- server.js
+++ server.js
@@ -45,7 +45,10 @@ chatHandler(io);
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('frontend'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(express.static('frontend'));
+}
 
 // API Routes
 app.use('/api/users', userRoutes);
